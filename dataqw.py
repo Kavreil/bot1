9dfa29b979e4759d9717dd332b00b56c0b92407f
Kavreil <komelin.98@mail.ru>
2024-10-15 18:55:12 +0300

    c:/Users/Kavreil/Desktop/git/bot1/bot_main.py Added h1 tag

---
+++ dataqw.py
@@ -0,0 +1,53 @@
+quiz_data = [
+    {
+        'question': 'Сколько змей в логотипе Python??',
+        'options': ['одна', 'две', 'три', 'их нет'],
+        'correct_option': 1
+    },
+    {
+        'question': 'Какая функция выводит что-либо в консоль?',
+        'options': ['print', 'out', 'cout', 'WriteLine'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Сколько библиотек можно импортировать в один проект?',
+        'options': ['8', '32', '16', 'неограниченно'],
+        'correct_option': 3
+    },
+    {
+        'question': 'Как получить данные от пользователя?',
+        'options': ['input', 'read', 'cin', 'get'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Что содержит в себе словарь?',
+        'options': ['Ключ', 'Значение', 'Значение и ключ', 'Список'],
+        'correct_option': 2
+    },
+    {
+        'question': 'Какой тип данных используется для хранения целых чисел?',
+        'options': ['int', 'float', 'str', 'natural'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Что такое Python?',
+        'options': ['Язык программирования', 'Тип данных', 'Музыкальный инструмент', 'Змея на английском'],
+        'correct_option': 0
+    },
+    {
+        'question': 'Как вывести список методов и атрибутов объекта "х"?',
+        'options': ['help', 'info', '?', 'dir'],
+        'correct_option': 3
+    },
+    {
+        'question': 'В каком году был разработан Python?',
+        'options': ['1996', '1991', '1988', '2000'],
+        'correct_option': 1
+    },
+    {
+        'question': 'Какой режим в команде open() позволяет записать данные в файл?',
+        'options': ['r', 'w', 'p', 'y'],
+        'correct_option': 1
+    },
+    # Добавьте другие вопросы
+]
